af906f2c469a8a8797e30d9d01b09de74489ee98
rjardem <rjardem@vmware.com>
2020-06-28 20:56:15 -0300

    move sut creation to a factory method

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,21 +1,25 @@
 const validator = require('validator')
 class EmailValidator {
   isValid (email) {
+    this.email = email
     return validator.isEmail(email)
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
 describe('Email validator', () => {
   console.error = jest.fn()
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@email.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Should return false if validator returns true', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@email.com')
     expect(isEmailValid).toBe(false)
   })
