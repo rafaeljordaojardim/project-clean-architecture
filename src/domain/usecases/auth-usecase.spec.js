efda77c70a0676689278830be4693e087ebc8b8a
rjardem <rjardem@vmware.com>
2020-07-01 06:25:58 -0300

    Ensure AuthUseCase Returns null when repository returns null

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -18,7 +18,10 @@ class AuthUseCase {
       throw new InvalidParamError('loadUserByEmailRepository')
     }
 
-    await this.loadUserByEmailRepository.load(email)
+    const user = await this.loadUserByEmailRepository.load(email)
+    if (!user) {
+      return null
+    }
   }
 }
 
@@ -26,6 +29,7 @@ const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
     async load (email) {
       this.email = email
+      return null
     }
   }
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
@@ -64,5 +68,11 @@ describe(('Auth Use Case'), () => {
     const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@anyemail.com', 'any_password')
     expect(promise).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
+  })
+
+  test('Should return null if LoadUserByEmailRepository returns null', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth('invalid_email@anyemail.com', 'any_password')
+    expect(accessToken).toBe(null)
   })
 })
