4a9ff059181fb51f3f863aa4f2a0c818a98303f0
rjardem <rjardem@vmware.com>
2020-07-03 07:16:14 -0300

    ensure encrypter throws erros if no params are provided

--- __mocks__/bcrypt.js
+++ __mocks__/bcrypt.js
@@ -1,8 +1,17 @@
+const MissingParamError = require('../src/utils/errors/missing-param-error')
+
 module.exports = {
   isValid: true,
   hash: '',
   value: '',
   async compare (value, hash) {
+    if (!value) {
+      throw new MissingParamError('value')
+    }
+
+    if (!hash) {
+      throw new MissingParamError('hash')
+    }
     this.value = value
     this.hash = hash
     return this.isValid
