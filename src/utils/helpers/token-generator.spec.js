9a5ef9ab46b25dd66fb8e823ba4ed349fb6fb418
rjardem <rjardem@vmware.com>
2020-07-03 21:24:58 -0300

    ensure token generator throws if no id is provided

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -10,6 +10,9 @@ class TokenGenerator {
     if (!this.secret) {
       throw new MissingParamError('secret')
     }
+    if (!id) {
+      throw new MissingParamError('id')
+    }
     return jwt.sign(id, this.secret)
   }
 }
@@ -22,13 +25,13 @@ describe(('Token Generator'), () => {
   test('Should return null if JWT returns null', async () => {
     const sut = makeSut()
     jwt.token = null
-    const token = await sut.generate('')
+    const token = await sut.generate('any_id')
     expect(token).toBeNull()
   })
 
   test('Should return a token if JWT returns a token', async () => {
     const sut = makeSut()
-    const token = await sut.generate('')
+    const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
   })
 
@@ -43,5 +46,11 @@ describe(('Token Generator'), () => {
     const sut = new TokenGenerator()
     const promise = sut.generate('any_id')
     expect(promise).rejects.toThrow(new MissingParamError('secret'))
+  })
+
+  test('Should throw if no id is provide', async () => {
+    const sut = makeSut()
+    const promise = sut.generate()
+    expect(promise).rejects.toThrow(new MissingParamError('id'))
   })
 })
