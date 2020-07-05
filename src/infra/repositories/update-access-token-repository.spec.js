cb36b66f45116d9ea57e4574bd54181c3dc7428e
rjardem <rjardem@vmware.com>
2020-07-05 15:41:30 -0300

    ensure UpdateAccessTOkenRepository throws erros if no userModel is provide

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -44,4 +44,18 @@ describe(('UpdateAccessToken Repository'), () => {
     const updatedFakeUser = await userModel.findOne({ _id: fakeUser.ops[0]._id })
     expect(updatedFakeUser.accessToken).toBe('valid_token')
   })
+
+  test('Should throw if no userModel is provide', async () => {
+    const userModel = db.collection('users')
+    const fakeUser = await userModel.insertOne({
+      email: 'valid_email@email.com',
+      name: 'any_name',
+      age: 50,
+      state: 'any_state',
+      password: 'hashed_password'
+    })
+    const sut = new UpdateAccessTokenRepository()
+    const promise = sut.update(fakeUser.ops[0]._id, 'valid_token')
+    expect(promise).rejects.toThrow()
+  })
 })
