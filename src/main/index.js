fc0e60366484f814531560994e1d7318bb82c0a5
rjardem <rjardem@vmware.com>
2020-07-06 06:38:28 -0300

    connect to mongodb before listen for app

--- src/main/index.js
+++ src/main/index.js
@@ -1,2 +1,8 @@
-const app = require('./config/app')
-app.listen(5858, () => console.log('Running'))
+const MongoHelper = require('../infra/helpers/mongo-helper')
+const env = require('./config/env')
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    const app = require('./config/app')
+    app.listen(5858, () => console.log('Running'))
+  })
+  .catch(console.error)
