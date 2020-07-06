f8e8d2f4212ceb7b0e2f05fa4215e30363b06b2e
rjardem <rjardem@vmware.com>
2020-07-05 21:27:58 -0300

    ensure request parse body as json

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,5 +1,7 @@
 const cors = require('../middlewares/cors')
+const jsonParser = require('../middlewares/json-parser')
 module.exports = app => {
   app.disable('x-powered-by')
   app.use(cors)
+  app.use(jsonParser)
 }
