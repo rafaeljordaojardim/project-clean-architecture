d8ae96b78974f0bb4735b268463010dcc79d36ae
rjardem <rjardem@vmware.com>
2020-07-05 21:47:43 -0300

    ensure requests return given content-type when force it

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -8,4 +8,13 @@ describe('Content-Type Middleware', () => {
     await request(app).get('/test_content_type')
       .expect('content-type', /json/)
   })
+
+  test('Should return xml content type if force it', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app).get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
 })
