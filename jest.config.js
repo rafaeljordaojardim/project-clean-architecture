8a53aa8841ce0d40ce3e79532caa278328420820
rjardem <rjardem@vmware.com>
2020-07-06 06:31:22 -0300

    ignore module main in covered

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@
 module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
   preset: '@shelf/jest-mongodb'
 }
