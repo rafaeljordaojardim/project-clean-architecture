600fc549f00a23892632a1f56e210562c55ae5be
rjardem <rjardem@vmware.com>
2020-07-08 06:43:03 -0300

    fiz mongodb port and app port

--- src/main/index.js
+++ src/main/index.js
@@ -3,6 +3,6 @@ const env = require('./config/env')
 MongoHelper.connect(env.mongoUrl)
   .then(() => {
     const app = require('./config/app')
-    app.listen(5858, () => console.log('Running'))
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
   })
   .catch(console.error)
